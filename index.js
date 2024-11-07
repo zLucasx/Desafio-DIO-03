a5915b2bf7700b0842ba7280903a9699d100363d
zLucasx <lucasglima2020@gmail.com>
2024-11-07 18:47:55 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+
+class Heroi {
+
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+
+        
+        switch (this.tipo.toLowerCase()) {
+            case 'mago':
+                ataque = 'magia';
+                break;
+            case 'guerreiro':
+                ataque = 'espada';
+                break;
+            case 'monge':
+                ataque = 'artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'shuriken';
+                break;
+            default:
+                ataque = 'um ataque desconhecido';
+        }
+
+        
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+
+const heroi1 = new Heroi("Lucas", 22, "guerreiro");
+
+heroi1.atacar();
